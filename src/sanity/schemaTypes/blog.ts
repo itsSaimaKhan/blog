7e650550176632438f3f8533c8a5777afd4aee6b
itssaimaKhan <samiakhan8754@gmail.com>
2025-01-07 00:42:26 +0500

    main

--- src/sanity/schemaTypes/blog.ts
+++ src/sanity/schemaTypes/blog.ts
@@ -1,24 +1,45 @@
-import { defineField, defineType, defineArrayMember } from "sanity"
+import { defineField, defineType, defineArrayMember } from "sanity";
 
-export const blog = defineType ({
-    name: 'blog',
-    title: 'Blog',
-    type: 'document',
-    
-    fields: [
-        defineField (
-    {
-        name: 'Title',
-        type: 'string',
-        title: 'Title',
-        
+export const blog = defineType({
+  name: "blog",
+  title: "Blog",
+  type: "document",
+
+  fields: [
+    defineField({
+      name: "Title",
+      type: "string",
+      title: "Title",
+    }),
+    defineField( {
+      name: "Paragraph",
+      type: "text",
+      title: "Paragraph",
+    }),
+    defineField({
+        name: 'slug',
+        type: 'slug',
+        title: 'Slug',
+        options:{
+            source:'Title'
+        }
 
     }),
-    {
-        name: 'Paragraph',
-        type: 'text',
-        title: 'Paragraph',
-    },
-]
+    defineField({
+        title: 'Image',
+        name: 'image',
+        type: 'image',
+        options: {
+            hotspot:true
+        }
+    }),
+    defineField({
+        title: 'Block',
+        name: 'block',
+        type: 'array',
+        of: [{type: 'block'}]
 
-})
+    })
+    
+  ]
+});
