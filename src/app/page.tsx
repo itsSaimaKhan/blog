586ee0d9b9d397e2a53757a1f2d84538fe037937
itssaimaKhan <samiakhan8754@gmail.com>
2025-01-08 02:23:05 +0500

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,58 @@
-import React from 'react'
-import Hero from './components/Hero'
-import { client } from '@/sanity/lib/client'
+// import React from 'react'
+// import Hero from './components/Hero'
+// import { client } from '@/sanity/lib/client'
 
 
+
+// const Homepage = async () => {
+//   const query = `*[_type == 'blog'] | order(_updateAt asc){
+//   Title,Paragraph,image,
+//   "slug":slug.current
+//   }`
+
+//   const data:Blog[] = await client.fetch(query)
+//   // console.log(data)
+
+//   return (
+
+//     <div >
+
+//       { data.map((data:Blog)=>(
+//           <Hero data={data} key={data.slug}/>
+//       )
+
+//       )}
+        
+//     </div>
+//   )
+// }
+
+// export default Homepage;
+
+import React from 'react';
+import Hero from './components/Hero';
+import { client } from '@/sanity/lib/client';
 
 const Homepage = async () => {
-  const query = `*[_type == 'blog'] | order(_updateAt asc){
-  Title,Paragraph,image,
-  "slug":slug.current
-  }`
+  const query = `*[_type == 'blog'] | order(_updatedAt asc){
+    Title, Paragraph, image,
+    "slug": slug.current
+  }`;
 
-  const data:Blog[] = await client.fetch(query)
-  // console.log(data)
+  const data: Blog[] = await client.fetch(query);
 
   return (
-
-    <div >
-
-      { data.map((data:Blog)=>(
-          <Hero data={data} key={data.slug}/>
-      )
-
-      )}
-        
+    <div className="flex overflow-x-auto space-x-6 p-6 scrollbar-hide">
+      {data.map((data: Blog) => (
+        <div
+          key={data.slug}
+          className="flex-shrink-0 w-[350px] h-[500px] border rounded-lg shadow-lg overflow-hidden bg-gray-800"
+        >
+          <Hero data={data} />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
